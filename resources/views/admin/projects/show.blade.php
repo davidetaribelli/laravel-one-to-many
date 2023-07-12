e6b4719dbad5ee4028f7b78b12ca577e6377bedb
davidetaribelli <davide.taribelli@gmail.com>
2023-07-12 10:00:39 +0200

    fix: add css in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,21 +4,15 @@
 <div class="container">
     <div class="row">
         <div class="col-12 d-flex flex-wrap justify-content-center g-4">
-            <div class="card mb-3" style="width: 35rem;">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="{{$project->thumb}}" class="img-fluid rounded-start" alt=""{{$project->title}}"">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$project->type->travel}}</h5>
-                      <h5 class="card-title">{{$project->title}}</h5>
-                      <p class="card-text">{{$project->description}}</p>
-                      <a class="card-text" href="{{$project->link}}"> <small class="text-muted">{{$project->link}}</small></a>
-                    </div>
-                  </div>
-                </div>
-            </div>
+          <div class="col-md-4">
+            <img src="{{$project->thumb}}" class="img-fluid rounded-start" alt=""{{$project->title}}"">
+          </div>    
+          <div class="col-md-5 mx-3 p-3 bg_darkorange rounded-3 text-white">
+            <h2 class="border-bottom my-2 py-2">{{$project->type->travel}}</h2>
+            <h3 class="border-bottom my-2 py-2">{{$project->title}}</h3>
+            <p class="fs-4 border-bottom my-2 py-2">{{$project->description}}</p>
+            <a class="text-decoration-none" href="{{$project->link}}"> <small class="text-white">{{$project->link}}</small></a>
+        </div>
         </div>
     </div>
     <div class="d-flex justify-content-center">
